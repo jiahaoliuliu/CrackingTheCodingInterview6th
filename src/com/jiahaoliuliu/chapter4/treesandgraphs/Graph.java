c4a377ec2707becfd9833d79660eadbbaf19c5f5
Jiahaoliuliu <jiahaoliuliu@gmail.com>
2022-01-28 00:49:52 +0400

    Added the question for Chapter 4 Q1: Routes between nodes

---
+++ src/com/jiahaoliuliu/chapter4/treesandgraphs/Graph.java
@@ -0,0 +1,17 @@
+package com.jiahaoliuliu.chapter4.treesandgraphs;
+
+enum State { Unvisited, Visited, Visiting}
+
+public class Graph {
+    public Node[] nodes;
+}
+
+class Node {
+    public String name;
+    public State state = State.Unvisited;
+    public Node[] children;
+
+    public Node(String name) {
+        this.name = name;
+    }
+}
